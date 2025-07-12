3e14456ca451b62fd9048546e6ab657f98843649
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-13 06:22:32 +0800

    新增登录界面

--- glancy-site/src/App.jsx
+++ glancy-site/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Login from './Login.jsx'
 
 function App() {
   return (
     <div className="App">
       <h1>Glancy 电子词典官网</h1>
       <p>欢迎来到 Glancy，这是一个专注于词汇学习的电子词典。</p>
+      <Login />
     </div>
   )
 }
