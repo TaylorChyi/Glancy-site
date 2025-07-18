d9344c33b777f2cac7397865e490d1827a2efb73
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-18 19:58:25 +0800

    [fix] bump version

--- glancy-site/src/main.jsx
+++ glancy-site/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './LanguageContext.jsx'
@@ -9,7 +10,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
       <ThemeProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </LanguageProvider>
   </StrictMode>,
