b11d39416781c18f42b0977f5f090791f784c94b
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-21 02:41:29 +0800

    [fix] persist login state

--- glancy-site/src/store/userStore.js
+++ glancy-site/src/store/userStore.js
@@ -1,7 +1,19 @@
 import { create } from 'zustand'
 
-export const useUserStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null })
-}))
+const STORAGE_KEY = 'user'
+
+export const useUserStore = create((set) => {
+  const stored = localStorage.getItem(STORAGE_KEY)
+  const initialUser = stored ? JSON.parse(stored) : null
+  return {
+    user: initialUser,
+    setUser: (user) => {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(user))
+      set({ user })
+    },
+    clearUser: () => {
+      localStorage.removeItem(STORAGE_KEY)
+      set({ user: null })
+    }
+  }
+})
