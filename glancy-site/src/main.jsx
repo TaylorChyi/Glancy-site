e830dca534c5289247f451cc1d1ce36b0090f366
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-21 02:41:13 +0800

    [fix] enable routing

--- glancy-site/src/main.jsx
+++ glancy-site/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Router from './Router.jsx'
 import { LanguageProvider } from './LanguageContext.jsx'
 import { ThemeProvider } from './ThemeContext.jsx'
 
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
     <LanguageProvider>
       <ThemeProvider>
         <BrowserRouter>
-          <App />
+          <Router />
         </BrowserRouter>
       </ThemeProvider>
     </LanguageProvider>
