e0dc9b4f833944a73e04ff81fa6f9ae4dc1751e2
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-21 20:25:55 +0800

    [fix] remove expanded favorites list

--- glancy-site/src/components/Sidebar/Favorites.jsx
+++ glancy-site/src/components/Sidebar/Favorites.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
 import { useLanguage } from '../../LanguageContext.jsx'
-import { useFavoritesStore } from '../../store/favoritesStore.js'
 import './Sidebar.css'
 
 function Favorites({ onToggle }) {
   const { t } = useLanguage()
-  const favorites = useFavoritesStore((s) => s.favorites)
-  const [open, setOpen] = useState(false)
 
   const handleClick = () => {
-    const next = !open
-    setOpen(next)
-    if (onToggle) onToggle(next)
+    if (onToggle) onToggle((v) => !v)
   }
 
   return (
-    <div className={`sidebar-section favorites-list${open ? ' expanded' : ''}`}> 
+    <div className="sidebar-section favorites-list">
       <h3 className="collection-button" onClick={handleClick}>
         {t.favorites || 'Favorites'}
       </h3>
-      {open && (
-        <ul>
-          {favorites.length ? (
-            favorites.map((w, i) => <li key={i}>{w}</li>)
-          ) : (
-            <li>No favorites yet.</li>
-          )}
-        </ul>
-      )}
     </div>
   )
 }
