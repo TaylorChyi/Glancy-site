aca96552b2b3c47b513bf9d2727b0a0bc8fc7a48
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-25 01:41:43 +0800

    [feature] unify top bar

--- glancy-site/src/components/TopBar.jsx
+++ glancy-site/src/components/TopBar.jsx
@@ -1,11 +1,12 @@
 import { useTheme } from '../ThemeContext.jsx'
 import { useLanguage } from '../LanguageContext.jsx'
+import { useIsMobile } from '../utils.js'
 import lightIcon from '../assets/glancy-web-light.svg'
 import darkIcon from '../assets/glancy-web-dark.svg'
 import TopBarActions from './TopBarActions.jsx'
-import './MobileTopBar.css'
+import './TopBar.css'
 
-function MobileTopBar({
+function TopBar({
   term = '',
   showBack = false,
   onBack,
@@ -14,12 +15,14 @@ function MobileTopBar({
   canFavorite = false,
   onOpenSidebar
 }) {
+  const isMobile = useIsMobile()
   const { resolvedTheme } = useTheme()
   const { lang } = useLanguage()
   const icon = resolvedTheme === 'dark' ? darkIcon : lightIcon
+  const text = term || (isMobile ? (lang === 'zh' ? '格律词典' : 'Glancy') : '')
 
   return (
-    <>
+    <header className="topbar">
       <button className="topbar-btn" onClick={onOpenSidebar}>
         <img src={icon} alt="brand" width={24} height={24} />
       </button>
@@ -34,14 +37,14 @@ function MobileTopBar({
       >
         ←
       </button>
-      <div className="term-text">{term || (lang === 'zh' ? '格律词典' : 'Glancy')}</div>
+      <div className="term-text">{text}</div>
       <TopBarActions
         favorited={favorited}
         onToggleFavorite={onToggleFavorite}
         canFavorite={canFavorite}
       />
-    </>
+    </header>
   )
 }
 
-export default MobileTopBar
+export default TopBar
