8e8ed487e39ead6b4cb57fa24c5b1212093cb45a
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-25 13:33:25 +0800

    [feature] add error boundary

--- glancy-site/src/main.jsx
+++ glancy-site/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 import './index.css'
 import App from './App.jsx'
 import Login from './Login.jsx'
@@ -22,11 +23,13 @@ createRoot(document.getElementById('root')).render(
       <LanguageProvider>
         <ThemeProvider>
           <BrowserRouter>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="*" element={<App />} />
-            </Routes>
+            <ErrorBoundary>
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<App />} />
+              </Routes>
+            </ErrorBoundary>
           </BrowserRouter>
         </ThemeProvider>
       </LanguageProvider>
